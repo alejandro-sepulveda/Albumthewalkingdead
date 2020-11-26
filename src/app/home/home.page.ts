3f6fcde08ef2c3ab1246fcf79af0e5c0f2d5abf0
unknown <alejandro_sepulveda16@hotmail.com>
2020-11-26 19:51:51 -0300

    subiendo primeros archivos

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,68 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  personajes= [
+    {
+      nombre: 'negan',
+      imagen: 'assets/recursos/negan.png',
+      sonido: 'assets/recursos/sonidos/negan.mp3'
+     
+
+    },
+
+    {
+      nombre: 'rick',
+      imagen: 'assets/recursos/rick.png',
+      sonido: 'assets/recursos/sonidos/rick.mp3'
+     
+
+    },
+
+    {
+      nombre: 'daryl',
+      imagen: 'assets/recursos/daryl.png',
+      sonido: 'assets/recursos/sonidos/daryl.mp3'
+     
+
+    },
+
+    {
+      nombre: 'carol',
+      imagen: 'assets/recursos/carol.png',
+      sonido: 'assets/recursos/sonidos/carol.mp3'
+     
+
+    },
+
+    {
+      nombre: 'michone',
+      imagen: 'assets/recursos/michone.png',
+      sonido: 'assets/recursos/sonidos/michone.mp3'
+     
+
+    },
+
+    {
+      nombre: 'carl',
+      imagen: 'assets/recursos/carl.jpg',
+      sonido: 'assets/recursos/sonidos/carl.mp3'
+     
+
+    },
+
+
+
+   
+   
+
+  ]
+
+  reproducir(personaje){
+    let sonido=new Audio();
+    sonido.src=personaje.sonido;
+    sonido.load();
+    sonido.play();
+
+  }
 
 }
